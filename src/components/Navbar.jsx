f4deadb1689ce2691cfd8f4c39cd53a641f1c6b2
Ashish Mishra <ashishgk1999@gmail.com>
2024-02-24 15:40:37 +0530

    navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+
+const link = [
+  {
+    id: 1,
+    item: "home",
+  },
+  {
+    id: 2,
+    item: "about",
+  },
+  {
+    id: 1,
+    item: "projects",
+  },
+  {
+    id: 1,
+    item: "contact",
+  },
+  {
+    id: 1,
+    item: "home",
+  },
+];
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  return (
+    <div className=" w-full h-20 flex justify-between items-center bg-gradient-to-r from-black to-gray-800 fixed">
+      <div>
+        <h1 className="font-signature text-5xl ml-2 text-white capitalize">
+          Ashish
+        </h1>
+      </div>
+      <ul className="hidden md:flex">
+        {link.map(({ id, item }) => (
+          <li
+            key={id}
+            className=" px-4 cursor-pointer hover:scale-105 duration-200 capitalize text-gray-600"
+          >
+            {item}
+          </li>
+        ))}
+      </ul>
+      <div
+        onClick={() => setNav(!nav)}
+        className="cursor-pointer text-white md:hidden pr-4 z-10"
+      >
+        {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
+      </div>
+      {nav && (
+        <ul className="flex flex-col justify-center items-center absolute top-0 left-0 w-full h-screen bg-gradient-to-b from-black to-gray-800">
+          {link.map(({ id, item }) => (
+            <li
+              key={id}
+              className="text-3xl py-6 px-4 cursor-pointer hover:scale-105 duration-200 capitalize text-gray-600"
+            >
+              {item}
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Navbar;
